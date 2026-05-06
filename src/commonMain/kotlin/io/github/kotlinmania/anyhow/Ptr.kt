241e7d52eb0e1887e6418480eee9dd8d651a3c5e
Sydney Renee <sydney@solace.ofharmony.ai>
2026-05-06 12:05:02 -0700

    Port ptr.rs

---
+++ src/commonMain/kotlin/io/github/kotlinmania/anyhow/Ptr.kt
@@ -0,0 +1,54 @@
+// port-lint: source src/ptr.rs
+package io.github.kotlinmania.anyhow
+
+public data class Own<T>(
+    public val ptr: T,
+) {
+    public companion object {
+        public fun <T> new(ptr: T): Own<T> = Own(ptr)
+    }
+
+    public fun <U> cast(): Own<U> = Own(ptr as U)
+
+    public fun boxed(): T = ptr
+
+    public fun byRef(): Ref<T> = Ref.fromRaw(ptr)
+
+    public fun byMut(): Mut<T> = Mut.fromRaw(ptr)
+}
+
+public data class Ref<T>(
+    public val ptr: T,
+) {
+    public companion object {
+        public fun <T> new(ptr: T): Ref<T> = Ref(ptr)
+
+        public fun <T> fromRaw(ptr: T): Ref<T> = Ref(ptr)
+    }
+
+    public fun <U> cast(): Ref<U> = Ref(ptr as U)
+
+    public fun byMut(): Mut<T> = Mut.fromRaw(ptr)
+
+    public fun asPtr(): T = ptr
+
+    public fun deref(): T = ptr
+}
+
+public data class Mut<T>(
+    public val ptr: T,
+) {
+    public fun <U> cast(): Mut<U> = Mut(ptr as U)
+
+    public fun byRef(): Ref<T> = Ref.fromRaw(ptr)
+
+    public fun extend(): Mut<T> = Mut(ptr)
+
+    public fun derefMut(): T = ptr
+
+    public fun read(): T = ptr
+}
+
+// Force turbofish on all calls of `.cast::<U>()`.
+public interface CastTo
+
